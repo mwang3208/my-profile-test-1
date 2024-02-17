770f97651462173682e6ab208b84a6ffee15703a
Kobus Van der Merwe <kobus@cs.utah.edu>
2024-02-17 12:46:35 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python
 
 tourDescription = """
-### MERIF 2023: Teaching 5G with POWDER and OpenAirInterface5G
+### Teaching 5G with POWDER and OpenAirInterface5G
 
-This profile is for an educational tutorial session during MERIF 2023. It
-deploys a single compute node with an image that includes docker,
+This profile is derived from a tutorial session created by Dustin Maas for MERIF 2023.
+It deploys a single compute node with an image that includes docker,
 docker-compose, tshark, oai-cn5g-fed v1.4.0, and docker images for all of the
 OAI 5G core network functions. It also includes source code and a prebuilt
-version of the OAI RAN stack (gNB, nrUE, RF simulator). We will show how
+version of the OAI RAN stack (gNB, nrUE, RF simulator). It shows how
 profiles like this one can be used in the classroom to explore 5G networks.
-While this session won't be a hands-on exercice, attendees will be able to try
-it out for themselves after the session, and use it as a starting point for
-teaching 5G concepts in their own classrooms. The description and instructions
-for this activity can be found
+
+The description and instructions for the original MERIF session can be found
 [here](https://gitlab.flux.utah.edu/powderrenewpublic/merif2023/-/blob/main/content/teaching-5g-oai.md).
 """
 
